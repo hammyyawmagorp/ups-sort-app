2cd9d083ebbc2725ac4cfde768af70e930293005
Hamal Samuel <zariktechonologies@gmail.com>
2022-09-03 23:06:49 -0400

    Updated Server JS

--- public/index.js
+++ public/index.js
@@ -1,8 +1,24 @@
-// for show/hide option on Hub Select Section
-document.querySelector("#name").addEventListener("change", function(){ 
-    document.querySelectorAll(".data").forEach(function(div){
-        div.style.display = "none";
-    })
-    document.querySelector("#" + this.value).style.display = "block";
-}); 
-document.querySelector("#name").dispatchEvent(new Event("change"));
+const hubInfoForm = document.querySelector('#hubInfoNasny');
+
+hubInfoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    getPD(hubInfoForm.elements.nasnyZip.value);
+    
+});
+
+// console.log(hubInfoForm.elements.nasnyZip);
+
+
+
+const localUrl = "http://localhost:5432"
+
+function getPD (zipcode){
+    axios.get (`${localUrl}/pd?zipcode=${zipcode}`)//?????????
+    .then((res) => {
+        document.querySelector('.pdDisplay').innerText = `The closest airport to your accomadations is: ${res.data[0].pd}`;
+        
+    }).catch(err => document.querySelector('.pdDisplay').innerText = "Offline"
+
+)}
+
+console.log()
---
+++ server/controller.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { CONNECTION_STRING } = process.env;
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+    dialect: 'postgres', 
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+module.exports = {
+    getPD: (req, res) => {
+    let { zipcode } = req.query;
+    sequelize.query(`
+    select color from nassau_ny_sort
+    where zip = ('${zipcode}');
+    `).then(dbRes => {res.status(200).send(dbRes[0])}).catch(err => console.log(err));
+    }
+
+};
